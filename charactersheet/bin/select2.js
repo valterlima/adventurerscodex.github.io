0640950690fc0df76c5bd20b2063128bb3f6b289
Brian Schrader <brian@brianschrader.com>
2016-06-07 20:10:14 -0700

    Release v0.0.10

--- charactersheet/bin/select2.js
+++
@@ -1,8 +0,0 @@
-ko.bindingHandlers.select2 = {
-    init: function(element, valueAccessor) {
-      var options = ko.toJS(valueAccessor()) || {};
-      setTimeout(function() {
-          $(element).select2(options);
-      }, 0);
-    }
-};
